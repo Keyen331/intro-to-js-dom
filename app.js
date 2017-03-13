fc0a499c827884fd9222f1dcdea1696395b8efea
Keyen331 <banayos.kay@gmail.com>
2017-03-13 08:34:00 -1000

    innitial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("sanity check");
+//finding html elements
+//nsole.log(ent.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+
+console.log(document.getElementsByTagName("div"));
+var tagElement = document.getElementsByTagName("div");
